e07780e143b8a8162bf20300bf5f34497d6e60a4
akisupi <mosesakisupi@gmail.com>
2024-09-01 01:11:41 +0000

    ---

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ import Image from "next/image";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52 my-auto">
         {/* <AcmeLogo /> */}
-        <Image src={MyLogo} height="50" width="50" alt="moses-logo" />
-        <h1 className="text-2xl font-bold px-2">MoseTech</h1>
+        <Image src={MyLogo} height="50" width="50" alt="moses-logo" className="lg:h-[100px] lg:w-[100px]" />
+        <h1 className="text-2xl lg:text-6xl font-bold px-2">MoseTech</h1>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
